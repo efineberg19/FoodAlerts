4316f44d3e6cd751b3adb3e8daf9117ad6b076e1
efineberg19 <bethrfineberg@gmail.com>
2019-10-26 12:07:27 -0500

    Made a class to get dining hall information from API

--- Twilio/run.py
+++ Twilio/run.py
@@ -3,8 +3,6 @@
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
 
-app = Flask(__name__)
-
 @app.route("/sms", methods=['GET', 'POST'])
 def sms_ahoy_reply():
     """Respond to incoming messages with a friendly SMS."""
